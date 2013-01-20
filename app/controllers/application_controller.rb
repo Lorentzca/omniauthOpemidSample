34f8673e672dd5138cede6706a42b4659c4c8150
cocoa <cocoa@cocoa-no-Mac-mini.local>
2013-01-20 09:13:12 +0900

    loginビュー編集。各編集箇所にコメント

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  #omniauth viewで使うcurrent_userの中身
+  #omniauth viewで使うcurrent_userメソッドの中身
+  #View内からコントローラーメソッドを呼びたいってこと
+  #単発のヘルパーはやだなって時とかこれで。
   helper_method :current_user
+  
   private
   def current_user
     @current_user ||= session[:user_info] if session[:user_info]
